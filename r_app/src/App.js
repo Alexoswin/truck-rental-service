9305c50e74890f82e53798578bb0cecc5dd8c33b
oswin <oswinalex1@gmail.com>
2023-10-11 02:36:02 +0530

    map

--- r_app/src/App.js
+++ r_app/src/App.js
@@ -30,9 +30,6 @@ import Login from './component/login';
 function App() {
 
 
-  // dark mode 
- 
-
   return (
     <>
       <Router>
@@ -44,13 +41,20 @@ function App() {
 
           <Route path="/Tracking" Component={Tracking} />
           <Route path="/Account" Component={Account}/>
-          <Route path ="/Details/truck1" element={<Opencard image={i1} />} />
-          <Route path ="/Details/truck2" element={<Opencard image={i2} />} />
-          <Route path ="/Details/truck3" element={<Opencard image={i3} />} />
-          <Route path ="/Details/truck4" element={<Opencard image={i4} />} />
-          <Route path ="/Details/truck5" element={<Opencard image={i5} />} />
-          <Route path ="/Details/truck6" element={<Opencard image={i6} />} />
-          <Route path ="/Details/truck7" element={<Opencard image={i7} />} />
+          <Route path ="/Details/truck1" element={<Opencard image={i1}
+           cost="8" text="Cost per Kilometer: $0.45 Capacity: 10 tons Description: Truck 1 is an economical choice for transporting goods. It offers a competitive cost per kilometer and can handle up to 10 tons of cargo. Experience the perfect blend of power and reliability with our range of trucks, designed to conquer any road or job. These robust vehicles offer exceptional performance, cutting-edge technology, and unmatched durability, making them the ideal choice for both work and play. Whether you're hauling heavy loads or embarking on an adventure, our trucks are your trusted companions on the road ahead" />} />
+          <Route path ="/Details/truck2" element={<Opencard image={i2}
+          cost="10" text="Cost per Kilometer: $0.50Capacity: 8 tonsDescription: Truck 2 provides a good balance between cost-efficiency and cargo capacity. With a cost per kilometer of $0.50, it can carry up to 8 tons of freight. Experience the perfect blend of power and reliability with our range of trucks, designed to conquer any road or job. These robust vehicles offer exceptional performance, cutting-edge technology, and unmatched durability, making them the ideal choice for both work and play. Whether you're hauling heavy loads or embarking on an adventure, our trucks are your trusted companions on the road ahead" />} />
+          <Route path ="/Details/truck3" element={<Opencard image={i3} 
+           cost="15" text="Cost per Kilometer: $0.55 Capacity: 12 tons Description: Truck 3 is a reliable choice for transporting heavy loads. While its cost per kilometer is slightly higher at $0.55, it boasts a generous capacity of 12 tons. Experience the perfect blend of power and reliability with our range of trucks, designed to conquer any road or job. These robust vehicles offer exceptional performance, cutting-edge technology, and unmatched durability, making them the ideal choice for both work and play. Whether you're hauling heavy loads or embarking on an adventure, our trucks are your trusted companions on the road ahead" />} />
+          <Route path ="/Details/truck4" element={<Opencard image={i4} 
+          cost="50" text="Cost per Kilometer: $0.60 Capacity: 6 tons Description: Truck 4 is a compact and cost-effective option for smaller cargo. It comes with a cost per kilometer of $0.60 and can carry up to 6 tons of goods. Experience the perfect blend of power and reliability with our range of trucks, designed to conquer any road or job. These robust vehicles offer exceptional performance, cutting-edge technology, and unmatched durability, making them the ideal choice for both work and play. Whether you're hauling heavy loads or embarking on an adventure, our trucks are your trusted companions on the road ahead" />} />
+          <Route path ="/Details/truck5" element={<Opencard image={i5} 
+          cost="67" text="Cost per Kilometer: $0.65 Capacity: 15 tons Description: Truck 5 is a heavy-duty hauler with a cost per kilometer of $0.65. It excels in transporting large loads, accommodating up to 15 tons of freight Experience the perfect blend of power and reliability with our range of trucks, designed to conquer any road or job. These robust vehicles offer exceptional performance, cutting-edge technology, and unmatched durability, making them the ideal choice for both work and play. Whether you're hauling heavy loads or embarking on an adventure, our trucks are your trusted companions on the road ahead" />} />
+          <Route path ="/Details/truck6" element={<Opencard image={i6} 
+          cost="100" text="Cost per Kilometer: $0.65 Capacity: 15 tons Description: Truck 5 is a heavy-duty hauler with a cost per kilometer of $0.65. It excels in transporting large loads, accommodating up to 15 tons of freight Experience the perfect blend of power and reliability with our range of trucks, designed to conquer any road or job. These robust vehicles offer exceptional performance, cutting-edge technology, and unmatched durability, making them the ideal choice for both work and play. Whether you're hauling heavy loads or embarking on an adventure, our trucks are your trusted companions on the road ahead" />} />
+          <Route path ="/Details/truck7" element={<Opencard image={i7}
+           cost="100" text="Cost per Kilometer: $0.65 Capacity: 15 tons Description: Truck 5 is a heavy-duty hauler with a cost per kilometer of $0.65. It excels in transporting large loads, accommodating up to 15 tons of freight Experience the perfect blend of power and reliability with our range of trucks, designed to conquer any road or job. These robust vehicles offer exceptional performance, cutting-edge technology, and unmatched durability, making them the ideal choice for both work and play. Whether you're hauling heavy loads or embarking on an adventure, our trucks are your trusted companions on the road ahead" />} />
           <Route path ="/login" Component={Login} />  
 
         </Routes>
--- r_app/src/component/when_carf_is_open.js
+++ r_app/src/component/when_carf_is_open.js
@@ -1,42 +1,63 @@
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-import React, { Component } from 'react'; // Import React and Component
-import mapboxgl from 'mapbox-gl'; // Import mapboxgl library
-
-class  Opencard extends Component {
-    componentDidMount() {
+class Opencard extends Component {
+  componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1Ijoib3N3aW5hbGV4IiwiYSI6ImNsbmV1endyMjBsNDEycXRjNmt4M2I3a28ifQ.M0WoyDbplPY3KfJKN5HZVg'; // Replace with your Mapbox access token
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [72.8777, 19.0760], // Default center (New York City)
-      zoom: 9.7
+      zoom: 9.7,
     });
 
     // Function to calculate the route
-    function calculateRoute() {
-      var startLocation = encodeURIComponent(document.getElementById('startLocation').value);
-      var endLocation = encodeURIComponent(document.getElementById('endLocation').value);
+    const calculateRoute = () => {
+      const startLocation = encodeURIComponent(
+        document.getElementById('startLocation').value
+      );
+      const endLocation = encodeURIComponent(
+        document.getElementById('endLocation').value
+      );
 
       // Use a geocoding service to convert place names to coordinates
-      fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${startLocation}.json?access_token=${mapboxgl.accessToken}`)
-        .then(response => response.json())
-        .then(startData => {
-          var startCoordinates = startData.features[0].geometry.coordinates;
+      fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${startLocation}.json?access_token=${mapboxgl.accessToken}`
+      )
+        .then((response) => response.json())
+        .then((startData) => {
+          const startCoordinates = startData.features[0].geometry.coordinates;
 
-          fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${endLocation}.json?access_token=${mapboxgl.accessToken}`)
-            .then(response => response.json())
-            .then(endData => {
-              var endCoordinates = endData.features[0].geometry.coordinates;
+          fetch(
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${endLocation}.json?access_token=${mapboxgl.accessToken}`
+          )
+            .then((response) => response.json())
+            .then((endData) => {
+              const endCoordinates = endData.features[0].geometry.coordinates;
 
               // Now, use the coordinates to fetch the route
-              fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${startCoordinates.join(',')};${endCoordinates.join(',')}?access_token=${mapboxgl.accessToken}`)
-                .then(response => response.json())
-                .then(data => {
+              fetch(
+                `https://api.mapbox.com/directions/v5/mapbox/driving/${startCoordinates.join(
+                  ','
+                )};${endCoordinates.join(',')}?access_token=${mapboxgl.accessToken}`
+              )
+                .then((response) => response.json())
+                .then((data) => {
                   if (data.routes && data.routes.length > 0) {
-                    var distance = (data.routes[0].distance / 1000).toFixed(2); // Convert meters to kilometers
-                    document.getElementById('totalDistance').textContent = distance;
-                    console.log(distance)
-                    var route = data.routes[0].geometry;
+                    const distance = (data.routes[0].distance / 1000).toFixed(2); // Convert meters to kilometers
+
+                    // Display the total distance
+                    document.getElementById('totalDistance').textContent = `Total Distance: ${distance} km`;
+
+                    // Calculate the total cost based on the cost per kilometer (from props)
+                    const costPerKm = this.props.cost; // Assuming cost is passed as a prop
+                    const totalCost = (distance * costPerKm).toFixed(2); // Calculate total cost
+
+                    // Display the total cost
+                    document.getElementById('totalCost').textContent = `Total Cost: ₹${totalCost}`;
+                    
+                    // Update the map route
+                    const route = data.routes[0].geometry;
                     if (!map.getSource('route')) {
                       map.addSource('route', {
                         type: 'geojson',
@@ -45,74 +66,111 @@ class  Opencard extends Component {
                           properties: {},
                           geometry: {
                             type: 'LineString',
-                            coordinates: []
-                          }
-                        }
+                            coordinates: [],
+                          },
+                        },
                       });
                     }
                     map.getSource('route').setData(route);
                   } else {
                     console.error('No route found.');
-                    document.getElementById('totalDistance').textContent = 'N/A';
+
+                    // Display N/A for distance and cost
+                    document.getElementById('totalDistance').textContent = 'Total Distance: N/A';
+                    document.getElementById('totalCost').textContent = 'Total Cost: ₹N/A';
                   }
                 })
-                .catch(error => {
+                .catch((error) => {
                   console.error('Error fetching route data:', error);
-                  document.getElementById('totalDistance').textContent = 'N/A';
+
+                  // Display N/A for distance and cost
+                  document.getElementById('totalDistance').textContent = 'Total Distance: N/A';
+                  document.getElementById('totalCost').textContent = 'Total Cost: ₹N/A';
                 });
             })
-            .catch(error => {
+            .catch((error) => {
               console.error('Error fetching end location coordinates:', error);
-              document.getElementById('totalDistance').textContent = 'N/A';
+
+              // Display N/A for distance and cost
+              document.getElementById('totalDistance').textContent = 'Total Distance: N/A';
+              document.getElementById('totalCost').textContent = 'Total Cost: ₹N/A';
             });
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error fetching start location coordinates:', error);
-          document.getElementById('totalDistance').textContent = 'N/A';
+
+          // Display N/A for distance and cost
+          document.getElementById('totalDistance').textContent = 'Total Distance: N/A';
+          document.getElementById('totalCost').textContent = 'Total Cost: ₹N/A';
         });
-    }
+    };
 
     // Attach the calculateRoute function to the button click event
     document.getElementById('calculateButton').addEventListener('click', calculateRoute);
-    
   }
-  
+  Openal=()=>{
+    alert('booked sucessfully')
+  }
+
   render() {
+    const { image, text } = this.props;
+
     return (
       <>
-        <div id="inputs">
-        <div id="map"></div>
-    
-       
-        <br/>
-        <div className="address">
-        <div id="services">Our services are all across Mumbai </div><br/><br/>
-          
-          <input type="text" class="route form-control" id="startLocation" placeholder="Enter start location" /><br/><br/>
-         
-          <input  type="text" aria-label="default input example" class="route form-control " id="endLocation" placeholder="Enter end location" /><br/><br/>
-          <button   class="size btn btn-success" id="calculateButton">Calculate Route</button>
-          <p id="distance">Total Distance: <span id="totalDistance">-</span> km</p>
-       
-                 
-              
-              <div class="input-group">
-            
-              <textarea  placeholder='Enter your address' class="form-control" aria-label="With textarea"></textarea>
-              
-            </div><br/>
-            <button  type="button" class=" size btn btn-success">Booknow</button>
-            
-          </div><br/>
-          
-
-       
+        <div id="col" className="aliceblue">
+          <div className="map_card_image">
+            <img className="map_card_style" alt="truck img" src={image} />
+            <div className="div-text">
+              <p className="paragraph">
+                {text}
+                <br />
+                <br />
+                <div className="cost_per_km">Cost per km = ₹{this.props.cost}</div>
+              </p>
+            </div>
+          </div>
+          <div id="inputs">
+            <div id="map"></div>
+            <br />
+            <div className="address">
+              <div id="services">Our services are all across Mumbai</div>
+              <br />
+              <br />
+              <input
+                type="text"
+                className="route form-control"
+                id="startLocation"
+                placeholder="Enter start location"
+              />
+              <br />
+              <input
+                type="text"
+                aria-label="default input example"
+                className="route form-control"
+                id="endLocation"
+                placeholder="Enter end location"
+              />
+              <br />
+              <button className="size btn btn-success" id="calculateButton">
+                Calculate Route
+              </button>
+              <p id="totalDistance">Total Distance: N/A</p>
+              <p id="totalCost">Total Cost: ₹N/A</p>
+              <div className="input-group">
+                <textarea
+                  placeholder="Enter your address"
+                  className="form-control"
+                  aria-label="With textarea"
+                ></textarea>
+              </div>
+              <br />
+              <button type="button" onClick={this.Openal} className="size btn btn-success">
+                Book Now
+              </button>
+            </div>
+            <br />
+          </div>
         </div>
-
-    
-        
-        
- 
       </>
     );
   }
